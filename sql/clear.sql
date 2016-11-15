f54179eedd9c91f54c25a527aa51992a2de10a6f
Ramie Raufdeen <me@ramie.ca>
2016-11-15 03:28:21 -0500

    script to clear tables

---
+++ sql/clear.sql
@@ -0,0 +1,11 @@
+use ece356db;
+
+set foreign_key_checks=0;
+drop table if exists Work_Address;
+drop table if exists Doctor;
+drop table if exists Patient_Address;
+drop table if exists Patient;
+drop table if exists Reviews;
+drop table if exists Specialization;
+drop table if exists Friend_Requests;
+set foreign_key_checks=1;
