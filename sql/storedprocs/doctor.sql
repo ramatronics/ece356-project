cd270580e750e14e51b5126671aaa016b8df4ad3
Ramie Raufdeen <me@ramie.ca>
2016-11-16 07:01:12 -0500

    doctor search query (still wit), and some fixes

--- sql/storedprocs/doctor.sql
+++ sql/storedprocs/doctor.sql
@@ -1,3 +1,26 @@
+-- SPLIT_STR MySQL Function
+-- from http://blog.fedecarg.com/2009/02/22/mysql-split-string-function/
+DROP FUNCTION IF EXISTS SPLIT_STR;
+CREATE FUNCTION SPLIT_STR(
+  x VARCHAR(255),
+  delim VARCHAR(12),
+  pos INT
+)
+RETURNS VARCHAR(255)
+RETURN REPLACE(SUBSTRING(SUBSTRING_INDEX(x, delim, pos),
+    LENGTH(SUBSTRING_INDEX(x, delim, pos -1)) + 1),
+    delim, '');
+
+/*
+Example:
+SELECT SPLIT_STR('a|bb|ccc|dd', '|', 3) as third;
++-------+
+| third |
++-------+
+| ccc   |
++-------+
+*/
+
 DROP PROCEDURE IF EXISTS CreateDoctor;
 DELIMITER @@
 CREATE PROCEDURE CreateDoctor
@@ -36,37 +59,33 @@ BEGIN
       );
   END IF;
 
-  SET @addr_id = (SELECT address_id
-                  FROM work_address wa
-                  WHERE
-                   wa.street_address = street_address AND
-                   wa.province = province AND
-                   wa.city = city AND
-                   wa.postal_code = postal_code);
-
   INSERT INTO doctor VALUES (
   	alias,
   	first_name,
   	last_name,
   	gender,
-    @addr_id,
-  	license
+    (SELECT address_id
+                    FROM work_address wa
+                    WHERE
+                     wa.street_address = street_address AND
+                     wa.province = province AND
+                     wa.city = city AND
+                     wa.postal_code = postal_code),
+  	licensed
   );
 
   SET @index = 0;
-
-	basic_loop:LOOP
-
+	specializationParseLoop:LOOP
   		SET @index = @index + 1;
-      SET @spec = SPLIT_STR(specializations, ",", @index);
+      SET @specialization = SPLIT_STR(specializations, ",", @index);
 
       IF @spec != '' THEN
-        insert into doctor_specialization(alias, specialization) values (alias, @spec);
-        ITERATE basic_loop;
+        INSERT INTO doctor_specialization(alias, specialization) values (alias, @specialization);
+        ITERATE specializationParseLoop;
       END IF;
 
-    leave basic_loop;
-	END LOOP basic_loop;
+    leave specializationParseLoop;
+	END LOOP specializationParseLoop;
 
 END @@
 DELIMITER ;
@@ -85,6 +104,38 @@ CREATE PROCEDURE DoctorSearch
    IN reviewed_by_friend BOOLEAN,
    IN caller_alias VARCHAR(20))
 BEGIN
+  SELECT d.alias, d.first_name, d.last_name, d.gender,
+         wa.city, wa.province, wa.postal_code,
+         avg(r.rating) AS avg_review_rating,
+         (YEAR(CURDATE()) - YEAR(d.license)) as years_licensed,
+         ds.specialization
+    FROM doctor d
+      INNER JOIN work_address wa ON wa.address_id = d.address_id
+      LEFT JOIN doctor_specialization ds ON ds.alias = d.alias
+      LEFT JOIN reviews r ON r.doctor_alias = d.alias
+    WHERE
+      (province IS NULL OR wa.province = province) AND
+      (city IS NULL OR wa.city = city) AND
+      (postal_code IS NULL OR wa.postal_code = postal_code) AND
+      (gender IS NULL OR d.gender = gender) AND
+      (specialization IS NULL OR ds.specialization = specialization) AND
+      (name_keyword IS NULL OR
+        ((d.first_name LIKE CONCAT('%', name_keyword, '%')) OR
+          (d.last_name LIKE CONCAT('%', name_keyword, '%')))
+      ) AND
+      (num_years_licensed IS NULL OR (YEAR(CURDATE()) - YEAR(d.license)) >  num_years_licensed) AND
+      (reviewed_by_friend IS FALSE OR EXISTS(
+        SELECT 1
+          FROM patient_friends pf
+            WHERE
+              ((pf.alias_from = caller_alias AND pf.alias_to IN (SELECT patient_alias FROM reviews))
+                OR
+                (pf.alias_to = caller_alias AND pf.alias_from IN (SELECT patient_alias FROM reviews))
+              ) AND pf.status = 1)
+      )
+      GROUP BY d.alias
+      HAVING (avg_star_rating_at_least IS NULL OR avg(r.rating) >= avg_star_rating_at_least);
+
   /* search for an exact match on province/city/postal_code/gender/specialization, substring
   match on name_keyword, inequality match on num_years_licensed and
   avg_star_rating_at_least, where a NULL value denotes a wildcard */
