668ae95792e05e82d68d47ea56a30fc47b9d1c91
Alex Ohn <alexohn3026@gmail.com>
2016-11-16 02:06:58 -0500

    AddReview

--- sql/storedprocs/reviews.sql
+++ sql/storedprocs/reviews.sql
@@ -10,6 +10,33 @@ CREATE PROCEDURE CreateReview
 BEGIN
   /* create a review using the given data */
   /* no return value */
+
+  SET @patient_exist = (SELECT count(*) from Patient where alias = patient_alias);
+  SET @doctor_exist = (SELECT count(*) from Doctor where alias = doctor_alias);
+
+  if (@patient_exists = 0 or @doctor_exits = 0 ) then
+    leave CreateReview;
+  end if;
+
+  set @valid_rate = MOD(star_rating, 0.5);
+
+  if(@valid_rating != 0 or star_rating > 5.0) then
+    leave CreateReview;
+  end if
+
+  insert into Review(
+    patient_alias,
+    doctor_alias,
+    created,
+    rating,
+    comments
+  ) values (
+    patient_alias,
+    doctor_alias,
+    NOW(),
+    star_rating,
+    comments
+  );
 END @@
 DELIMITER ;
 
